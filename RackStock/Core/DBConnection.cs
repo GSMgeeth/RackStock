c13c3d89b7c1a1354752d65343be436d604096b9
Geeth sandaru Madhushan <geeth.gsm@gmail.com>
2018-09-23 06:34:28 +0530

    Initialized System connections and references

---
+++ RackStock/Core/DBConnection.cs
@@ -0,0 +1,101 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RackStock.Core
+{
+    class DBConnection
+    {
+        private static string connString = string.Format("Server=localhost; database=rack_database; UID=root; password=; SSLMode=none");
+        private static MySqlConnection conn = new MySqlConnection(connString);
+        private static MySqlConnection tmpConn = null;
+
+        private DBConnection() { }
+
+        private static MySqlConnection getConnection()
+        {
+            try
+            {
+                if (conn != null)
+                {
+                    if (conn.State.ToString().Equals("closed") || conn.State.ToString().Equals("Closed"))
+                        conn.Open();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Connection error!\nCheck if the MySQL connection has started!", "MySQL Connection", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+
+            return conn;
+        }
+
+        private static MySqlConnection getTmpConnection()
+        {
+            if (tmpConn == null)
+            {
+                tmpConn = new MySqlConnection(connString);
+            }
+
+            if (tmpConn.State.ToString().Equals("Closed") || tmpConn.State.ToString().Equals("closed"))
+                tmpConn.Open();
+
+            return tmpConn;
+        }
+
+        public static MySqlDataReader getDataViaTmpConnection(string qry)
+        {
+            return new MySqlCommand(qry, getTmpConnection()).ExecuteReader();
+        }
+
+        public static void closeTmpConnection()
+        {
+            if (tmpConn.State.ToString().Equals("Open") || tmpConn.State.ToString().Equals("open"))
+                tmpConn.Close();
+        }
+
+        public static MySqlDataReader getData(string qry)
+        {
+            if (conn != null)
+                return new MySqlCommand(qry, getConnection()).ExecuteReader();
+            else
+                return null;
+        }
+
+        public static void updateDB(string qry)
+        {
+            if (conn != null)
+            {
+                new MySqlCommand(qry, getConnection()).ExecuteNonQuery();
+            }
+        }
+
+        public static void backupDB()
+        {
+            try
+            {
+                string file = "D:/PackingSocks/Backup.sql";
+
+                if (conn != null)
+                {
+                    using (MySqlCommand cmd = new MySqlCommand())
+                    {
+                        using (MySqlBackup mb = new MySqlBackup(cmd))
+                        {
+                            cmd.Connection = getConnection();
+                            mb.ExportToFile(file);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ RackStock/Core/Database.cs
@@ -0,0 +1,15 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RackStock.Core
+{
+    class Database
+    {
+        
+    }
+}
